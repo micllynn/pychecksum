f37e1a72ed316bd17158a257488439419d715daa
Michael Lynn <michaellynn@mac-dpag1305.dpag.ox.ac.uk>
2024-06-03 16:47:45 +0100

    new class to track local/server directory pair, revised readme

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Introduction
 
-pychecksum contains a set of simple tools to efficiently compute and
-compare checksums of files or folders.
+pychecksum contains a set of tracking and checksum calculation tools to
+verify the data integrity of folders that are being synchronized from local
+hard drives to a server.
 
-It is designed to be used if large folders are being copied to a server,
-but there is some possibility of corruption. By comparing checksums
-of the original and copied folders, users can decide whether any files
-are corrupted and whether the folder copy operation needs to be rerun.
+Folders are tracked and verified in an object-oriented framework,
+and if checksum calculations don't match, the new subfolders
+can be automatically deleted for subsequent resynchronization
 
 # Installation
 ```python3
@@ -16,51 +16,65 @@ python3 -m pip install .
 ```
 
 # Usage
+First, we initialize a sync object to track a folder with a local and server copy.
+This performs a diff of the directory trees to see which folders are only
+present on the local drive (ie will be synchronized to the server).
 ```python3
 import pychecksum as pycs
 
-result = pycs.compare_folder_checksums(
-	'localdrive/folder_original',
-	'backupdrive/folder_copy')
+syncobj = pycs.FolderSyncObj(
+	'/localdrive/folder_original',
+	'/backupdrive/folder_copy')
 ```
 
-This will output `result=True` if the checksums for each file in the folder match,
-and `result=False ` otherwise.
-
-# More advanced options:
-
-## Change the checksum type
-By default, pychecksum uses the sha256 hash type. This can be changed
-to any hash type found in hashlib, including md5:
-```python3
-result = pycs.compare_folder_checksums(
-	'localdrive/folder_original',
-	'backupdrive/folder_copy',
-	checksum_type=hashlib.md5)
+Next, we perform some synchronization event in the terminal:
+```bash
+>>> rsync, rclone, robocopy, etc. to synchronize local and server folders
 ```
 
-## Output more information about checksums that don't match
-`verbose=True` will print an update of which files are currently being
-checksum-verified, and which ones the checksum doesn't match for.
-`verbose_folder_checksums=True` will print a granular update of the checksum
-values for each individual file in the folder
+We can then use the `syncobj` to verify the checksums of the subset of new folders.
+- Note that `rm_folders_if_integrity_bad` will automatically remove the folders that
+have failed the checksum calculation (with a prompt), for ease of resynchronization.
+```python3
+output = syncobj.verify_checksums(
+	checksum_type=hashlib.sha256,
+	rm_folders_if_integrity_bad=True)
+```
+
+```bash
+local folder: /localdrive/folder_original...
+server folder: /backupdrive/folder_copy...
+comparing checksums...
+        script1.py...	checksum matches.
+        script2.py...	 ***** checksum does not match ***** 
+----------
+
+**** file integrity compromised during transfer **** 
+removing folders on server...
+	are you sure you want to delete /backupdrive/folder_copy? (y/n) y
+		deleted /backupdrive/folder_copy
+
+```
+
+The `output` contains a more granular list of checksum verification for each file:
+```python3
+print(output.paths)
+>> {'/localdrive/folder_original': False}  # indicates this fodler failed the checksum calc.
+print(output.transfer_ok)
+>> False  # indicates at least one folder failed the checksum calc.
+```
+
+# More advanced functions:
+## `pycs.compare_folder_checksums`
+This allows for a manual checksum calculation between folders.
 
 ```python3
 result = pycs.compare_folder_checksums(
-	'localdrive/folder_original',
-	'backupdrive/folder_copy',
+	'folder1',
+	'folder2',
+	checksum_type=hashlib.md5	
 	verbose=True,
 	verbose_folder_checksums=False)
-```
-
-`verbose` output:
-```
-comparing checksums...
-	file1.tiff...	checksum matches.
-	file2.tiff...	checksum matches.
-	file3.tiff...	 ***** checksum does not match *****
-	file4.tiff...	checksum matches.
-	file5.tiff...	checksum matches.
 ```
 
 `verbose_folder_checksums` output:
@@ -72,9 +86,10 @@ computing checksum of folder_original/file2.tiff...
 	sha256 checksum: 4523496bd224607ea9f5283494534bcffaf9270e976936c35ca8e3819f85df1e
 ```
 
-## Get checksum of individual files or folders
-For more granular control, this outputs the actual checksum for a particular
-named file:
+## `pycs.get_checksum` and `pycs.get_folder_checksum`
+These functions output checksums for individual files and folders respectively.
+
+Checksum of a single file:
 ```python3
 result = pycs.get_checksum(
 	'localdrive/folder_original/some_file.tiff'
@@ -82,7 +97,7 @@ result = pycs.get_checksum(
 	verbose=True)
 ```
 
-And a dictionary of checksum values for each file in a folder:
+This returns a dictionary of checksum values for each file in a folder:
 ```python3
 result = pycs.get_folder_checksum(
 	'localdrive/folder_original'
--- pychecksum.py
+++ pychecksum.py
@@ -12,10 +12,236 @@ decide whether to re-run the backup or not).
 (Written for Lak Lab)
 """
 import hashlib
+import pathlib
+import filecmp
+import shutil
 import os
 import sys
+from types import SimpleNamespace
 
-def get_checksum(fname, checksum_type=hashlib.md5,
+
+class TrackDir(object):
+    def __init__(self, path):
+        """An object for keeping track of directories
+        before and after significant changes (eg copy from
+        local drive to server).
+
+        - ls_new() returns a set of PathObjects
+        in directory that has changed from when the DirCompare
+        instance was initialized to the present
+
+        - rm_new() removes all directories that have changed
+        from when the DirCompare instance was initialized to
+        the present
+
+        Parameters
+        ------------
+        directory : str
+            Path to a directory to monitor
+        """
+        self.path = path
+        self.path_obj = SimpleNamespace()
+        self.folders = SimpleNamespace()
+
+        self.path_obj.pre = pathlib.Path(self.path)
+        self.folders.pre = [x for x in
+                            self.path_obj.pre.rglob('*') if x.is_dir()]
+
+    def _log_state(self):
+        """Logs the state of the filetree within self.directory"""
+        self.path_obj.post = pathlib.Path(self.path)
+        self.folders.post = [x for x in
+                             self.path_obj.post.rglob('*') if x.is_dir()]
+
+    def ls_new(self):
+        """Returns a list of directories that are new
+        in the filetree of self.directory since the object
+        was initialized"""
+        self._log_state()
+
+        set_pre = set(self.folders.pre)
+        set_post = set(self.folders.post)
+
+        folder_diffs = list(set_post - set_pre)
+
+        return folder_diffs
+
+    def rm_new(self):
+        """Removes all new directories
+        in the filetree of self.directory since the object
+        was initialized"""
+        folder_diffs = self.ls_new()
+
+        for folder_diff in folder_diffs:
+            shutil.rmtree(folder_diff, ignore_errors=True)
+
+        return
+
+
+class FolderSyncObj(object):
+    def __init__(self, dir_local, dir_server):
+        """
+        Object to track a folder which will be synchronized between a
+        local drive and the server.
+
+        Two directories should be provided, a local and server path.
+        These should be identical except that the local
+        copy should have more folders that are not on the server yet
+        (but will be during an upcoming transfer operation.)
+
+        Expected usage:
+        =============
+        syncobj = FolderSyncObj(dir_local='/Users/test/folder_localcopy',
+                                dir_local='/Server/folder_servercopy')
+        >> # do some sync stuff like rsync, robocopy, etc. between the folders
+
+        output = syncobj.verify_checksums(verbose=True)
+        >> comparing checksums...
+        >> 	script1.py...	checksum matches.
+        >> 	script2.py...	 ***** checksum does not match ***** 
+
+        print(output.transfer_ok)
+        >> False
+
+        Parameters
+        ----------
+        dir_local : str
+            String corresponding to local directory
+        dir_server : str
+            String corresponding to server directory
+        """
+
+        # setup
+        self.path = SimpleNamespace()
+        self.path.local = dir_local
+        self.path.server = dir_server
+
+        self.path_compare = filecmp.dircmp(self.path.local,
+                                      self.path.server)
+
+        self.diff_fullpath = SimpleNamespace()
+        self.diff_fullpath.local = self.get_diffs_localpaths()
+        self.diff_fullpath.server = self.get_diffs_serverpaths()
+
+    def list_diffs(self):
+        """
+        List all folders that are only in local, not in server
+        (lists relative paths to dir_local, not complete path)
+        """
+        return self.path_compare.left_only
+
+    def get_diffs_localpaths(self):
+        """
+        For all folders that are different (ie only on local),
+        get the full local paths for these folders and store in a list
+        """
+        local_folders = self.list_diffs()
+        local_folders_fullpath = []
+        for local_folder in local_folders:
+            local_folders_fullpath.append(
+                os.path.join(self.path.local, local_folder))
+
+        return local_folders_fullpath
+
+    def get_diffs_serverpaths(self):
+        """
+        For all folders that are different (ie only on local),
+        get the full expected server paths for these folders and store in a list.
+
+        (Expected server paths are generated based on the root server path provided.
+        """
+        local_folders = self.list_diffs()
+        server_folders_fullpath = []
+        for local_folder in local_folders:
+            server_folders_fullpath.append(os.path.join(self.path.server, local_folder))
+
+        return server_folders_fullpath
+
+    def verify_checksums(self, checksum_type=hashlib.sha256,
+                         verbose=True, verbose_folder_checksums=False,
+                         rm_folders_if_integrity_bad=False,
+                         ask_before_rm=True):
+        """Call this method after the files have been transferred from local
+        to server. Performs checksum computation on all files that have been
+        newly transferred from local to server, prints updates on whether
+        the checksums match, and returns an object that summarizes
+        the checksum computation.
+
+        Parameters
+        -----------
+        checksum_type : str
+            Can be hashlib.md5,
+            hashlib.sha1, hashlib.sha256, etc.
+            (Note that you merely need to provide a class, no need to initialize
+            with brackets)
+        verbse : bool
+            Whether to print updates or not
+        verbose_folder_checksums : bool
+            Whether to print granular updates about the individual folder checksums
+
+        Returns
+        -----------
+        checksum_obj : SimpleNamespace
+            checksum_obj.paths : dict
+                Comprises of a dictionary of named local directories
+                and whether they are correctly transferred (True) or not (False)
+            checksum_obj.transfer_ok : bool
+                Returns True if all folders are transferred correclty,
+                False otherwise
+        """
+
+        checksum_obj = SimpleNamespace()
+        checksum_obj.paths = {}
+        checksum_obj.transfer_ok = True
+
+        for ind, local_folder in enumerate(self.diff_fullpath.local):
+            if verbose == True:
+                print(f'local folder: {self.diff_fullpath.local[ind]}...')
+                print(f'server folder: {self.diff_fullpath.server[ind]}...')
+            checksum_obj.paths[local_folder] = compare_folder_checksums(
+                self.diff_fullpath.local[ind],
+                self.diff_fullpath.server[ind],
+                checksum_type=checksum_type,
+                verbose=verbose,
+                verbose_folder_checksums=verbose_folder_checksums)
+
+            if checksum_obj.paths[local_folder] == False:
+                 checksum_obj.transfer_ok = False
+            print('------------')
+
+        if checksum_obj.transfer_ok == False:
+            if verbose == True:
+                print('\n**** file integrity compromised during transfer **** ')
+            if rm_folders_if_integrity_bad == True:
+                print('removing folders on server...')
+                self.rm_new_folders(ask_before_rm=ask_before_rm)
+        elif checksum_obj.transfer_ok == True:
+            if verbose == True:
+                print('**** file integrity ok ****')
+            
+        return checksum_obj
+
+    def rm_new_folders(self, ask_before_rm=True):
+        """Removes all new folders in Server that were created during the
+        transfer process (ie when the folder was started to be tracked)
+        """
+        diffs_serverpath = self.get_diffs_serverpaths()
+
+        for path in diffs_serverpath:
+            if ask_before_rm is True:
+                check = input(f'\tare you sure you want to delete {path}? (y/n)')
+                if check == 'y':
+                    shutil.rmtree(path, ignore_errors=True)
+                    print(f'\t\tdeleted {path}')
+                else:
+                    pass
+            elif ask_before_rm is False:
+                shutil.rmtree(path, ignore_errors=True)
+                print(f'\tdeleted {path}')
+        return
+
+
+def get_checksum(fname, checksum_type=hashlib.sha256,
                  verbose=False):
     """
     computes checksum of a given file.
@@ -46,7 +272,8 @@ def get_checksum(fname, checksum_type=hashlib.md5,
     return checksum.hexdigest()
 
 
-def get_folder_checksum(folder, checksum_type=hashlib.md5,
+def get_folder_checksum(folder, checksum_type=hashlib.sha256,
+                        recursive=False,
                         verbose=False):
     """
     calls get_checksum on all elements of a folder, and stores the outputs
@@ -62,19 +289,33 @@ def get_folder_checksum(folder, checksum_type=hashlib.md5,
     verbose : bool
         Whether to print updates or not
     """
-    fnames = os.listdir(folder)
-    checksums = {}
+    if recursive is False:
+        fnames = os.listdir(folder)
+        checksums = {}
 
-    for fname in fnames:
-        try:
-            checksums[fname] = get_checksum(os.path.join(folder, fname),
-                                            checksum_type=checksum_type,
-                                            verbose=verbose)
-            if verbose is True:
-                print('')  # necessary to skip a line
-        except IsADirectoryError:
-            if verbose is True:
-                print(f'{fname} is a directory, passing...\n')
+        for fname in fnames:
+            try:
+                checksums[fname] = get_checksum(os.path.join(folder, fname),
+                                                checksum_type=checksum_type,
+                                                verbose=verbose)
+                if verbose is True:
+                    print('')  # necessary to skip a line
+            except IsADirectoryError:
+                if verbose is True:
+                    print(f'{fname} is a directory, passing...\n')
+
+    if recursive is True:
+        path_obj = pathlib.Path(folder)
+        path_files = [x for x in path_obj.rglob('*') if x.is_file()]
+        checksums = {}
+        checksums_pathobj = []
+
+        for path_file in path_files:
+            checksums[path_file.name] = get_checksum(
+                path_file,
+                checksum_type=checksum_type,
+                verbose=verbose)
+            # checksums_pathobj.extend(path_file)
 
     return checksums
 
